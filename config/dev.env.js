3b2cc7b8094a6b4b7638f927e08a692872efe006
Bigbrother6 <820839841@qq.com>
2019-08-07 10:24:41 +0800

    update

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  url1:'"https://v1.itooi.cn"',
-  url2:'"https://v1.itooi.cn"',
+  url1:'"https://v1.itooi.cn/netease"', //网易接口
+  url2:'"https://v1.itooi.cn"',        //qq接口
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  url1:'"https://v1.itooi.cn"',
-	url2:'"https://v1.itooi.cn"',
+  url1:'"https://v1.itooi.cn/netease"',//网易接口
+	url2:'"https://v1.itooi.cn"',       //qq
 }
